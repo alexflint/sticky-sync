565fe00b4d109f3e7ce444cd58c648725ca6029f
alexflint <alex.flint@gmail.com>
2014-01-19 12:37:59 -0500

    updated deploy_to_aws.sh

--- scripts/deploy_to_aws.sh
+++ scripts/deploy_to_aws.sh
@@ -2,4 +2,4 @@
 
 AWS_STICKYSYNC=ec2-54-200-19-165.us-west-2.compute.amazonaws.com
 
-ssh $AWS_STICKYSYNC "cd sticky-sync && git pull && sh scripts/restart_production_server.sh"
+ssh $AWS_STICKYSYNC "cd sticky-sync && sh scripts/stop_production_server.sh && git pull && sh scripts/start_production_server.sh"
